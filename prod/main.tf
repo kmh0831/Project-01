6b1f7e1dd53cd9139ecf5bae90efccd543d67f74
kmh0831 <kmhyuk1018@gmail.com>
2024-09-04 11:04:56 +0900

    Deploy

--- prod/main.tf
+++ prod/main.tf
@@ -30,49 +30,49 @@ module "webserver_cluster" {
   alb_sg_name            = var.alb_sg_name
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.webserver_cluster.eks_cluster_endpoint
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(module.webserver_cluster.eks_cluster_certificate_authority_data)
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.webserver_cluster.eks_cluster_endpoint
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#     cluster_ca_certificate = base64decode(module.webserver_cluster.eks_cluster_certificate_authority_data)
+#   }
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.webserver_cluster.eks_cluster_name
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.webserver_cluster.eks_cluster_name
+# }
 
-resource "helm_release" "cluster_autoscaler" {
-  depends_on = [module.webserver_cluster]
+# resource "helm_release" "cluster_autoscaler" {
+#   depends_on = [module.webserver_cluster]
 
-  name       = "cluster-autoscaler"
-  repository = "https://kubernetes.github.io/autoscaler"
-  chart      = "cluster-autoscaler"
-  namespace  = "kube-system"
+#   name       = "cluster-autoscaler"
+#   repository = "https://kubernetes.github.io/autoscaler"
+#   chart      = "cluster-autoscaler"
+#   namespace  = "kube-system"
 
-  set {
-    name  = "autoDiscovery.clusterName"
-    value = var.cluster_name
-  }
+#   set {
+#     name  = "autoDiscovery.clusterName"
+#     value = var.cluster_name
+#   }
 
-  set {
-    name  = "awsRegion"
-    value = var.aws_region
-  }
+#   set {
+#     name  = "awsRegion"
+#     value = var.aws_region
+#   }
 
-  set {
-    name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.webserver_cluster.cluster_autoscaler_role_arn
-  }
+#   set {
+#     name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = module.webserver_cluster.cluster_autoscaler_role_arn
+#   }
 
-  set {
-    name  = "extraArgs.scale-down-unneeded-time"
-    value = "10m"
-  }
+#   set {
+#     name  = "extraArgs.scale-down-unneeded-time"
+#     value = "10m"
+#   }
 
-  set {
-    name  = "extraArgs.scale-down-delay-after-add"
-    value = "10m"
-  }
-}
+#   set {
+#     name  = "extraArgs.scale-down-delay-after-add"
+#     value = "10m"
+#   }
+# }
 
